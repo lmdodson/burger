61f7cb3f024d28e9e8faa0cbfa8243e4423d418c
Leah Dodson <leahdodson9@gmail.com>
2020-04-22 19:03:43 -0500

    built db connection and orm

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql");
+
+//Set up JAWSDB for heroku deployment or localhost for local connection
+if (process.env.JAWSDB_URL) {
+	var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	var connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "",
+		password: "",
+		database: "burgers_db",
+	});
+}
+
+// Establish connection
+connection.connect(function (err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for ORM
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,95 @@
+// Import mysql connection
+const connection = require("./connection.js");
+
+// Helper function for sql syntax
+function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	}
+	return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+	var arr = [];
+
+	// loop through the keys and push the key/value as a string int arr
+	for (var key in ob) {
+		var value = ob[key];
+		// check to skip hidden properties
+		if (Object.hasOwnProperty.call(ob, key)) {
+			// if string with spaces, add quotations
+			if (typeof value === "string" && value.indexOf(" ") >= 0) {
+				value = "'" + value + "'";
+			}
+			arr.push(key + "=" + value);
+		}
+	}
+
+	// translate array of strings to a single comma-separated string
+	return arr.toString();
+}
+
+// Object for all SQL statement functions
+const orm = {
+	selectAll: function (tableInput, cb) {
+		// define query string
+		var queryString = "SELECT * FROM " + tableInput + ";";
+		// handle errors
+		connection.query(queryString, function (err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+
+	insertOne: function (table, cols, vals, cb) {
+		// define query string
+		var queryString = "INSERT INTO " + table;
+
+		// define query string values
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
+
+		console.log(queryString);
+
+		// handle errors
+		connection.query(queryString, vals, function (err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+	updateOne: function (table, objColVals, condition, cb) {
+		// define query string
+		var queryString = "UPDATE " + table;
+
+		// define query string values
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
+
+		console.log(queryString);
+
+		// handle errors
+		connection.query(queryString, function (err, result) {
+			if (err) {
+				throw err;
+			}
+
+			cb(result);
+		});
+	},
+};
+
+// Export the orm object for the model
+module.exports = orm;
