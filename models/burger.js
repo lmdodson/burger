b4bb804885e4b8b972f3f3446f78ccd1fd490cb1
Leah Dodson <leahdodson9@gmail.com>
2020-04-22 19:19:47 -0500

    built out the db models

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import the ORM
+const orm = require("../config/orm.js");
+
+// Database interaction functions
+var burger = {
+	selectAll: function (cb) {
+		orm.selectAll("burgers", function (res) {
+			cb(res);
+		});
+	},
+	insertOne: function (cols, vals, cb) {
+		orm.insertOne("burgers", cols, vals, function (res) {
+			cb(res);
+		});
+	},
+	updateOne: function (objColVals, condition, cb) {
+		orm.updateOne("burgers", objColVals, condition, function (res) {
+			cb(res);
+		});
+	},
+};
+
+// Export the database functions for the controller
+module.exports = burger;
